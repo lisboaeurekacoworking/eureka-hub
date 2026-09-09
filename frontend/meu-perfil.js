4b11c7c903ca7cfa393bc3d1340804a279a0af60
lisboaeurekacoworking <lisboaeurekacoworking@gmail.com>
2026-09-09 16:43:16 +0100

    Adiciona meu perfil e faz mudanças de estilo

---
+++ frontend/meu-perfil.js
@@ -0,0 +1,9 @@
+const meuPerfil = {
+  nome: "Luiza",
+  cidade: "Lisboa",
+  area: "Customer Experience",
+};
+
+document.getElementById("inputNome").value = meuPerfil.nome;
+document.getElementById("inputCidade").value = meuPerfil.cidade;
+document.getElementById("inputArea").value = meuPerfil.area;
